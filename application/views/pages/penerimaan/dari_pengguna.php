1339608104ff08824a2e1e166728a38ee2c45c25
kantorujbitdev <146151378+kantorujbitdev@users.noreply.github.com>
2026-03-03 10:56:43 +0700

    reset

--- application/views/pages/penerimaan/dari_pengguna.php
+++ application/views/pages/penerimaan/dari_pengguna.php
@@ -51,7 +51,7 @@
                                     <button type="submit" class="btn btn-primary">
                                         <i class="fas fa-search"></i> Tampilkan
                                     </button>
-                                    <a href="<?= site_url('penerimaan/dari_supplier') ?>" class="btn btn-secondary">
+                                    <a href="<?= site_url('penerimaan/dari_pengguna') ?>" class="btn btn-secondary">
                                         <i class="fas fa-sync"></i> Reset
                                     </a>
                                 </div>
